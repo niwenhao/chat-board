001cd7f2c984e4ee92948f4611d18a7f35762f91
niwenhao <niwenhao@gmail.com>
2025-02-02 23:34:23 +0900

    u

--- app/context/AuthContext.js
+++ app/context/AuthContext.js
@@ -4,14 +4,16 @@ import { createContext, useState } from "react";
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-    const [username, setUsername] = useState("");
-    const [address, setAddress] = useState("");
-    const [email, setEmail] = useState("");
-    const [tel, setTel] = useState("");
+  const [username, setUsername] = useState("");
+  const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
+  const [tel, setTel] = useState("");
 
-    return (<AuthContext.Provider value={{ username, setUsername, address, setAddress, email, setEmail, tel, setTel }}>
-        {children}
-    </AuthContext.Provider>);
+  return (
+    <AuthContext.Provider value={{ username, setUsername, address, setAddress, email, setEmail, tel, setTel }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export { AuthContext, AuthProvider };
